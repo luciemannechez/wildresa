567882a32e26b44276a4ecf2192e2ce7b2b8d84e
Lucie Mannechez <luciem92@gmail.com>
2015-10-29 17:11:49 +0100

    Add list of events in admin with filters

---
+++ src/Wcs/WildResaBundle/Admin/EventsAdmin.php
@@ -0,0 +1,44 @@
+<?php
+// src/Acme/DemoBundle/Admin/PostAdmin.php
+
+namespace Wcs\WildResaBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class EventsAdmin extends Admin
+{
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('start', 'doctrine_orm_date_range')
+            ->add('machines', null, array(), null, array('expanded' => true, 'multiple' => true))
+            ->add('user')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->add('start','datetime')
+            ->add('end','datetime')
+            ->add('machines')
+            ->add('user')
+        ;
+    }
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        // to remove a single route
+        $collection->remove('delete');
+        // OR remove all route except named ones
+        $collection->clearExcept(array('list', 'show'));
+    }
+}
